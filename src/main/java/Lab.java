1cdad4dbad33d5e7765ec765bfc927e6d86ec0d2
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-20 21:19:11 +0000

    lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,9 +1,13 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Queues are FIFO - first in, first out, like a checkout line. They are used in algorithms where it is needed to keep
- * track of the order that the elements were inserted in. For instance, tech support tickets may be
+ * Queues are FIFO - first in, first out, like a checkout line. They are 
+ * used in algorithms where it is needed to keep
+ * track of the order that the elements were inserted in. For instance, tech 
+ * support tickets may be
  * waiting in a queue until a tech support specialist can clear them.
  * 
  * For instance, 
@@ -16,27 +20,36 @@ import java.util.Queue;
  * 2 | 3 |  
  * 3 |   |  
  *
- * ArrayDeque fills the role of *both* queues and stacks. You could think of a Deque as a deck of cards (pronounced the
- * same) where you could remove cards  from either the top or bottom. You can use ArrayDeque
- * to perform queue operations. You can read about ArrayDeque in the official Javadocs here:
+ * ArrayDeque fills the role of *both* queues and stacks. You could think of 
+ * a Deque as a deck of cards (pronounced the
+ * same) where you could remove cards  from either the top or bottom. You can
+ *  use ArrayDeque
+ * to perform queue operations. You can read about ArrayDeque in the official
+ *  Javadocs here:
  * https://docs.oracle.com/javase/7/docs/api/java/util/ArrayDeque.html
  * And you can read about the queue interface here:
  * https://docs.oracle.com/javase/8/docs/api/java/util/Queue.html
  * Notice that ArrayDeque implements multiple interfaces, including Queue!
  *
- * PriorityQueue is another implementation of Queue that has additional functionality
- * to handle multiple 'priorities' of items. You can read about PriorityQueue here:
+ * PriorityQueue is another implementation of Queue that has additional 
+ * functionality
+ * to handle multiple 'priorities' of items. You can read about PriorityQueue 
+ * here:
  * https://www.geeksforgeeks.org/priority-queue-class-in-java/
  */
 public class Lab {
     /**
-     * Instantiate and return some type of Deque, which is a class that contains Queue and Stack functionality.
-     * @return an instantiated Deque. Queue, by itself, doesn't have a class, so you should use a class that
-     * implements the Queue interface, such as ArrayDeque. Deques contain all the methods required for both Queue and
+     * Instantiate and return some type of Deque, which is a class that 
+     * contains Queue and Stack functionality.
+     * @return an instantiated Deque. Queue, by itself, doesn't have a class,
+     * so you should use a class that
+     * implements the Queue interface, such as ArrayDeque. Deques contain all 
+     * the methods required for both Queue and
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        Deque<String> que = new ArrayDeque <> ();
+        return que;
     }
 
     /**
@@ -45,7 +58,8 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        // queue = new ArrayDeque<>();
+        return queue.size();
     }
 
     /**
@@ -55,7 +69,8 @@ public class Lab {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+        // queue = new ArrayDeque<String>();
+        queue.add(item);
     }
 
     /**
@@ -65,7 +80,8 @@ public class Lab {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        // queue = new LinkedList<>();
+        return queue.poll();
     }
 
     /**
@@ -75,6 +91,7 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        // queue = new ArrayDeque<>();
+        return queue.peek();
     }
 }
